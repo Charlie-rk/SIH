ed17bc48f227ad5c4b7ad1d833a18dc21f5e9856
sangam2ishra <sangamkr.mishra@gmail.com>
2024-12-03 21:33:14 +0530

    updated nodes with notification

--- api/models/NodeModel.js
+++ api/models/NodeModel.js
@@ -36,6 +36,19 @@ const NodeSchema = new mongoose.Schema({
   },
   L1Connections: [{ type: String }], // Links to other Level 1 hubs (using strings)
   L2Connections: [{ type: String }], // Linked Level 2 nodes (using strings)
+  notifications: [
+    {
+      notificationId: { type: String, ref: "Notification" }, // Reference to the Notification schema
+      parcelId: { type: String, required: true }, // Parcel associated with the notification
+      message: { type: String, required: true }, // Message content
+      status: { 
+        type: String, 
+        enum: ["Sent", "Pending"], 
+        default: "Pending" 
+      },
+      timestamp: { type: Date, default: Date.now },
+    }
+  ],
 }, { timestamps: true });
 
 const Node = mongoose.model("Node", NodeSchema);
